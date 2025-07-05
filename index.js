c3d435db3479b206a6c0ba835f8a1108a2635d88
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2025-07-05 10:46:49 +0600

    get touristspot section added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const { ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,6 +32,21 @@ async function run() {
          const cursor = touristSpotCollection.find();
          const result = await cursor.toArray();
          res.send(result);
+      });
+
+      app.get("/touristsSpot/:id", async (req, res) => {
+         const id = req.params.id;
+         try {
+            const query = { _id: new ObjectId(id) };
+            const result = await touristSpotCollection.findOne(query);
+            if (!result) {
+               return res.status(404).send({ error: "Tourist spot not found" });
+            }
+            res.send(result);
+         } catch (error) {
+            console.error(error);
+            res.status(500).send({ error: "Server error" });
+         }
       });
 
       app.post("/touristsSpot", async (req, res) => {
