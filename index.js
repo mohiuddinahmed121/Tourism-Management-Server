2d4404006163b9bb195cfe98d7477beaa9c05a6a
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2025-07-08 00:30:22 +0600

    delete functionality added

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0lug6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
    serverApi: {
       version: ServerApiVersion.v1,
@@ -23,7 +22,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
    try {
-      // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
 
       const touristSpotCollection = client.db("touristDB").collection("touristSpot");
@@ -65,11 +63,17 @@ async function run() {
             res.status(500).json({ error: "Failed to fetch user" });
          }
       });
+
+      app.delete("/touristsSpot/:id", async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id) };
+         const result = await touristSpotCollection.deleteOne(query);
+         res.send(result);
+      });
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
    } finally {
-      // Ensures that the client will close when you finish/error
       // await client.close();
    }
 }
